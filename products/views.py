563f4f43e85abf3904e322e1170a3a1a36396259
Yasir Mahabub <yasirmahabub@gmail.com>
2025-03-12 21:39:51 +0600

    add home page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Count
+from .models import Product, Category
 
-# Create your views here.
+
+def home(request):
+    products = Product.objects.all()
+    categories = Category.objects.annotate(product_count=Count('products'))
+    
+    context={
+        'products': products,
+        'categories': categories
+    }
+    
+    return render(request, 'products/home.html', context)
