ddafcbdf6cad816a1dd38f351c4effeb8fefb2f0
Yasir Mahabub <yasirmahabub@gmail.com>
2025-03-17 02:31:09 +0600

    remove unnecessary clean_email method

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,9 +10,3 @@ class CustomUserRegistrationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ["first_name", "last_name", "email", "password1", "password2"]
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        if CustomUser.objects.filter(email=email).exists():
-            raise forms.ValidationError("This email is already in use.")
-        return email
