fb2b5acbe2472aadf830208c82e5cbf4a91e0b7d
Yasir Mahabub <yasirmahabub@gmail.com>
2025-03-17 03:20:33 +0600

    fix password hashing issue in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if form.cleaned_data.get("password"):
+            obj.set_password(form.cleaned_data["password"])
+        super().save_model(request, obj, form, change)
