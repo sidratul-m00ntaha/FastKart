305e4f325cfb64719ff83659ea515fbf442dbd4f
Yasir Mahabub <yasirmahabub@gmail.com>
2025-03-17 02:34:49 +0600

    remove email attribute definition as the attributes can be derived from the model class

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,9 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 from .models import CustomUser
 
 
 class CustomUserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
     class Meta:
         model = CustomUser
         fields = ["first_name", "last_name", "email", "password1", "password2"]
